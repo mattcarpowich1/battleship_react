fdecb991389da5edddf8087a8f2d7b56d35373e0
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-02-08 05:21:03 -0800

    removed commented out constants

--- client/src/constants/ShipConstants.js
+++ client/src/constants/ShipConstants.js
@@ -4,12 +4,10 @@ export const ORIENTATIONS = [HORIZONTAL, VERTICAL]
 export const [
   PATROL_BOAT,
   SUBMARINE
-  // AIRCRAFT_CARRIER
 ] = [0, 1]
 export const SHIP_TYPES = [
   PATROL_BOAT,
   SUBMARINE
-  // AIRCRAFT_CARRIER
 ]
 export const SHIP_SIZES = [2, 3]
 export const DEFAULT_COORDINATES = [
