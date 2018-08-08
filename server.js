99b40771b95e2b9255768ba0b902bfbcca30dc5b
mattcarpowich1 <mattcarpowich@gmail.com>
2018-08-07 17:07:06 -0700

    added server and procfile

---
+++ server.js
@@ -0,0 +1,18 @@
+const express    = require('express');
+const path       = require ('path');
+const app        = express();
+const PORT       = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('*', function(req, res) {
+  res.sendFile('./index.html');
+});
+
+app.listen(PORT);
