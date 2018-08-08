0aff6653bb447d06fa30915a02f9db05acebb882
mattcarpowich1 <mattcarpowich@gmail.com>
2018-08-07 17:23:52 -0700

    uncomment build from client gitignore

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ const PORT       = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use('*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
